c38c0206a08efa635bcf202cfbc6628bc2a56e06
meng <mlz070613@163.com>
2022-08-24 17:28:41 +0800

    封装工具函数更新

--- README.md
+++ README.md
@@ -26,3 +26,47 @@ JS学习
 
 - `null`表示被分配了一块空内存，生成一个哈希地址
 - `undefined`表示声明的变量为赋值，参与运算的时候，表达式的结果是`NaN`
+
+#### 4.循环一个不断变换的结构
+
+- 例：`NodeList`集合类型是实时的，文档结构的变化会实时地在他们身上反映出来，因此他们的值始终代表最新的状态。
+
+- 基于此，下面的循环会导致无限循环
+
+  ```js
+  let divs = document.getElementsByTagName('div')
+  for(let i = 0;i<divs.length;i++){
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+  }
+  ```
+
+
+- 每次循环开始，都会求值`i < divs.length`。这意味着要执行获取所有`<div>` 元素的查询。因为循环体中会创建并向文档添加一个新的`<div>` 元素，所以每次循环`divs.length`的值也会递增。因为两个值都会递增，所以i将永远不会等于`dis.length`，导致无限循环。
+
+- 解决办法：任何时候要迭代NodeList，最好在初始化一个变量保存当时查询时的长度，然后用循环变量与这个变量进行比较。
+
+  ```js
+  let divs = document.getElementsByTagName('div')
+  for(let i = 0;len = divs.length;i<len;i++){
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+  }
+  ```
+
+
+- 在这个例子中，又初始化了一个保存集合长度的变量`len`。因为`len`保存着循环开始时集合的长度，而这个值不会随着集合增大动态增长，所以这就可以避免无限循环。
+
+
+- 另外，如果不想在初始化一个变量，也可以反向迭代集合。
+
+  ```js
+  let divs = document.getElementsByTagName('div')
+  for(let i = divs.length - 1;i >= 0;i--){
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+  }
+  ```
+
+  ​
+
--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,17 @@
-// 随机数
+/**
+ * 随机数
+ * @param {Number} num1 起始数值
+ * @param {Number} num2 结束数值
+ */
 function getRandomNumber(num1, num2) {
   let total_num = num2 - num1 + 1
   return Math.floor(Math.random() * total_num + num1)
 }
 
-// 时间格式化
+/**
+ * 时间格式化
+ * @param {String} time 毫秒数
+ */
 function dateFormatting(time) {
   let YY = time.getFullYear()
   let MM = time.getMonth() + 1
@@ -15,7 +22,10 @@ function dateFormatting(time) {
   return `${YY}-${MM}-${DD} ${hh}:${mm}:${ss}`
 }
 
-// 时间补零格式化
+/**
+ * 时间补零格式化
+ * @param {String} time 毫秒数
+ */
 function dateZeroFormat(time) {
   //计算天数
   d = parseInt(time / 60 / 60 / 24)
@@ -33,7 +43,10 @@ function dateZeroFormat(time) {
   return `${d}:${h}:${m}:${s}`
 }
 
-// 检测浏览器插件
+/**
+ * 检测浏览器插件
+ * @param {String} name 浏览器插件名字
+ */
 function hasPlugin(name) {
   name = name.toLowerCase()
   // window.navigator.plugins 和name属性已被弃用
@@ -45,7 +58,11 @@ function hasPlugin(name) {
   return false
 }
 
-// 获取样式
+/**
+ * 获取样式
+ * @param {Object} element 获取样式的元素
+ * @param {String} attr 要获取的样式
+ */
 function getStyle(element, attr) {
   if (window.getComputedStyle) {
     return window.getComputedStyle(element)[attr]
@@ -54,7 +71,10 @@ function getStyle(element, attr) {
   }
 }
 
-// location查询字符串格式化
+/**
+ * location查询字符串格式化
+ * @param {String} location 传入的location地址
+ */
 function getQueryStringArgs(location) {
   let qs = location.search.length > 0 ? location.search.substring(1) : ''
   let args = {}
@@ -66,4 +86,106 @@ function getQueryStringArgs(location) {
     }
   }
   return args
+}
+
+/**
+ * 获取元素上的所有属性并格式化
+ * @param {Object} element 传入的要获取属性的元素
+ */
+function outputAttributes(element) {
+  let pairs = []
+  for (let i = 0; i < element.attributes.length; i++) {
+    const attribute = element.attributes[i]
+    pairs.push(`${attribute.nodeName}="${attribute.nodeValue}"`)
+  }
+  return pairs.join(' ')
+}
+
+/**
+ * 动态添加外部js文件
+ * @param {String} url 传入的url地址
+ */
+function loadScript(url) {
+  let script = document.createElement('script')
+  script.src = url
+  document.body.appendChild(script)
+}
+
+/**
+ * 动态嵌入js源代码
+ * @param {String} code 传入的javascript代码
+ */
+function loadScriptString(code) {
+  var script = document.createElement('script')
+  script.type = 'text/javascript'
+  try {
+    // 在标准浏览器中可以运行，但在旧版本的IE中可能会导致问题。IE对script元素作了处理，不允许常规DOM访问其子节点
+    script.appendChild(document.createTextNode(code))
+  } catch (ex) {
+    // 在旧版本的IE中，script元素上有一个text属性可以用来添加javascript代码
+    script.text = code
+  }
+  document.body.appendChild(script)
+}
+// loadScriptString("function sayHi(){alert('hi');}");
+
+/**
+ * 动态添加外部css样式表
+ * @param {String} url 传入的url地址
+ */
+function loadStyles(url) {
+  let link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = url
+  let head = document.getElementsByTagName('head')[0]
+  head.appendChild(link)
+}
+
+/**
+ * 动态嵌入css源代码
+ * @param {String} css 传入的css样式代码
+ */
+function loadStyleString(css) {
+  let style = document.createElement('style')
+  style.type = 'text/css'
+  try {
+    style.appendChild(document.createTextNode(css))
+  } catch (ex) {
+    style.styleSheet.cssText = css
+  }
+  let head = document.getElementsByTagName('head')[0]
+  head.appendChild(style)
+}
+// loadStyleString("body{background-color:red}");
+
+/**
+ * 防抖
+ * @param {Function} func 要进行防抖的函数
+ * @param {String} delay 延迟的毫秒数
+ * @param {Boolean} immediate 表示是否立即执行
+ */
+function debounce(func, delay, immediate = true) {
+  let timer
+
+  return function (...args) {
+    if (timer) clearTimeout(timer)
+
+    // 默认立即执行方法，延后执行的话，会让人感觉有卡顿
+    if (immediate) {
+      // 定义现在是否能执行
+      let now = !timer
+      if (now) func.apply(this, args)
+      // 不论timeout有没有值，都重新给timeout新添加一个定时器
+      // 等待wait时间后，将timeout设为null，代表可以继续执行次function
+      timer = setTimeout(() => {
+        timer = null
+      }, delay)
+    } else {
+      // 如果不是立即执行此函数，则在等待 delay 延迟时间后执行方法
+      timer = setTimeout(() => {
+        func.apply(this, args)
+      }, delay)
+    }
+  }
 }
